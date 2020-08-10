e7bf31ea39b209d9b7f9d31af06ca986b4d81463
Haris Rahman <haris.rahman@larisin.id>
2020-08-10 11:20:09 +0700

    Add dummy response data

---
+++ src/dummy-data/category-popular.js
@@ -0,0 +1,20 @@
+const dummyCategories = [
+  {
+    category: 'Self Development',
+    line: '/image/Line1.png',
+  },
+  {
+    category: 'Engineering',
+    line: '/image/Line2.png',
+  },
+  {
+    category: 'Novel',
+    line: '/image/Line3.png',
+  },
+  {
+    category: 'Computer Science',
+    line: '/image/Line4.png',
+  },
+];
+
+export default dummyCategories;
---
+++ src/dummy-data/item-product.js
@@ -0,0 +1,38 @@
+const dummyResponse = [
+  {
+    imgurl: '/image/listBook/1.png',
+    title: 'The Subtle Art Not Giving a F*ck',
+    writer: 'Mark Watson',
+    price: 100000,
+    discount_price: 10,
+  },
+  {
+    imgurl: '/image/listBook/2.png',
+    title: 'Start With Why',
+    writer: 'Simon Sinek',
+    price: 221000,
+    discount_price: 10,
+  },
+  {
+    imgurl: '/image/listBook/3.png',
+    title: 'The Design Every day',
+    writer: 'Mark Watson',
+    price: 221000,
+    discount_price: 10,
+  },
+  {
+    imgurl: '/image/listBook/ikigai.png',
+    title: 'Steal like artist jurnal',
+    writer: 'Austin Kleon',
+    price: 142000,
+    discount_price: 10,
+  },
+  {
+    imgurl: '/image/listBook/download.png',
+    title: 'Steal like artist jurnal',
+    writer: 'Austin Kleon',
+    price: 142000,
+    discount_price: 10,
+  },
+];
+export default dummyResponse;
